93bb9b3b4d08e9da252678146e2984c1040820ca
itsmattpaw <itsmattpaw@gmail.com>
2022-02-21 14:49:40 -0500

    commit tree 88a519a4ef37c250e608301f2b03625ad2bc5ebf

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    #initialiaze school
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    ##add students and create grade keys if none exists already
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+        roster[grade] ||= roster[grade] = []
+        roster[grade] << name
+    end
+    #grade method should put out all students from that grade
+    def grade(display_grade)
+        @roster[display_grade]
+    end
+    #create sorted list of all students
+    def sort
+        @roster.each{|key, val| @roster[key].sort!}
+    end
+end
